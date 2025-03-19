6fee907ce4f13a0474ac517dda0133fc78cc478d
cindy-pan-ics4u1-02-25 <cindy.pan@student.tdsb.on.ca>
2025-03-19 14:48:51 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,41 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
 
+	public static int find (int[] array, int number){
+		int index = -1;
+		for (int i =0; i<array.length; i++){
+			if (number == array[i]){
+				index = i;
+				i = array.length;
+			}
+		}
+		return index;
 	}
 
+	public static int findLast(String[] arr, String text){
+		int index = -1;
+		for (int i = 0; i < arr.length; i++){
+			if (text.equals(arr[i])){
+				index = i;
+			}	
+		}
+		return index;
+	}
+
+	public static int findSecond(char[] ar, char ch){
+		int index = -1;
+		int count = 0;
+		for (int i = 0; i<ar.length; i++){
+			if (count == 2){
+				i = ar.length;
+			}
+			else if (ch == ar[i]){
+				index = i;
+				count++;
+			}
+		}
+		return index;
+	}
 	
 
 }
